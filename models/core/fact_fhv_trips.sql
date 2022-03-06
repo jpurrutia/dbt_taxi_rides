85e6e907dd14c6eb71ecbfe3df91f11953b0fe67
Juan Pablo <jpurrutia95@gmail.com>
2022-03-06 21:40:06 +0000

    add dim to fhv_trips

--- models/core/fact_fhv_trips.sql
+++ models/core/fact_fhv_trips.sql
@@ -1,5 +1,27 @@
 {{ config(materialized='table') }}
 
+with fhv_tripdata as (
 select
 *
 from {{ ref('stg_fhv_tripdata') }}
+),
+
+dim_zones as (
+    select * from {{ ref('dim_zones') }}
+    where borough != 'Unknown'
+)
+select 
+    fhv_tripdata.trip_id,
+    fhv_tripdata.dispatching_base_num,
+    fhv_tripdata.pickup_locationid,
+    fhv_tripdata.dropoff_locationid,
+    fhv_tripdata.pickup_datetime,
+    fhv_tripdata.dropoff_datetime,
+    fhv_tripdata.sr_flag,
+    pickup_zone.borough as pickup_borough, 
+    pickup_zone.zone as pickup_zone
+from fhv_tripdata
+inner join dim_zones as pickup_zone
+on fhv_tripdata.pickup_locationid = pickup_zone.locationid
+inner join dim_zones as dropoff_zone
+on fhv_tripdata.dropoff_locationid = dropoff_zone.locationid
